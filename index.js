d0cfb58b37a207f8df0f29059a26b2ff843e7ca0
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-08 20:27:46 -0500

    dependencies

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require('./routes/users');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.once('open', function() {
+    console.log('Connected to MongoDB');
+}).on('error', function(error){
+    console.log('Connection error:', error);
+});
+
+// Middleware
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome to homepage');
+//     });
+// app.get('/users', (req, res) => {
+//     res.send('Welcome to users page');
+//     });
+
+app.listen(4400, () => {
+  console.log('Server is running on port 4400');
+});
