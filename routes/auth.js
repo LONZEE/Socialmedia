d78ccc245f398701bfeaae722e6a0f6128798617
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-05-09 16:51:39 -0500

    updated User.js,auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,14 +7,14 @@ router.post('/register', async (req, res) => {
     //generatae new password
     try {
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);     //hash password
         
         
         //create user
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: hashedPassword,
+            password: hashedPassword,   //save hashed password
         });
         //save user and respond
         const savedUser = await newUser.save();
@@ -31,7 +31,7 @@ router.post('/login', async (req, res) => {
         const user = await User.findOne({ email: req.body.email });
         !user && res.status(404).json('user not found');
 
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        const validPassword = await bcrypt.compare(req.body.password, user.password); //comparing password with user password
         !validPassword && res.status(400).json('wrong password');
 
         res.status(200).json(user);
